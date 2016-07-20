7bdcfa559e18eea1ba992ee572d42570d375197e
AdriVig <adrien.vigour@gmail.com>
2016-07-20 16:50:56 +0200

    Ajout check email valide
    Ajout check email valide

--- src/DH/PlatformBundle/Resources/public/js/scripts.js
+++ src/DH/PlatformBundle/Resources/public/js/scripts.js
@@ -1,5 +1,10 @@
 $(window).load(function() {
 
+	function isValidEmailAddress(emailAddress) {
+		var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+		return pattern.test(emailAddress);
+	};
+
 	$('#fos_user_registration_form_submit').click(function () {
 		var username = $('#fos_user_registration_form_username').val();
 		var email = $('#fos_user_registration_form_email').val();
@@ -18,7 +23,9 @@ $(window).load(function() {
 						$('#availability_result').html('L\'adresse email ' + email + ' est déjà utilisée, veuillez en prendre une autre.');
 					else
 						$('#availability_result').html('Tout les champs sont obligatoires.');
-				} else if (pwd != pwdv)
+				} else if (!isValidEmailAddress(email))
+					$('#availability_result').html('Veuillez rentrer une adresse email valide.');
+				else if (pwd != pwdv)
 					$('#availability_result').html('Les mots de passe doivent être identiques.');
 				else if (pwd.length < 8 || pwd.length > 42)
 					$('#availability_result').html('Le mot de passe doit contenir entre 8 et 42 caractères.');
